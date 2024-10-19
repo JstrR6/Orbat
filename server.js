4e57f4a8bcb9bba96f1c25438b62afc4514334d6
JstrR6 <tigerspires13@gmail.com>
2024-10-19 12:35:54 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const { Client, GatewayIntentBits } = require('discord.js');
+const { Client, GatewayIntentBits, Collection } = require('discord.js');
 const dotenv = require('dotenv');
 
 // Load environment variables
@@ -12,6 +12,21 @@ const client = new Client({
     GatewayIntentBits.MessageContent,
   ],
 });
+
+// Define the military ranks
+const militaryRanks = [
+  'Private', 'Private First Class', 'Specialist', 'Corporal', 'Sergeant', 
+  'Staff Sergeant', 'Sergeant First Class', 'Master Sergeant', 'First Sergeant', 
+  'Sergeant Major', 'Command Sergeant Major', 'Sergeant Major of the Armed Forces', 
+  'Second Lieutenant', 'First Lieutenant', 'Captain', 'Major', 'Lieutenant Colonel', 
+  'Colonel', 'Brigadier General', 'Major General', 'Lieutenant General', 'General', 
+  'General of the Armed Forces'
+];
+
+// Function to get roles that match military ranks
+function getMilitaryRoles(guild) {
+  return guild.roles.cache.filter(role => militaryRanks.includes(role.name));
+}
 
 // When the client is ready, run this code (only once)
 client.once('ready', () => {
@@ -34,6 +49,14 @@ client.on('messageCreate', async (message) => {
     } else if (command === 'orbat') {
       // TODO: Implement ORBAT command
       await message.reply('ORBAT command not yet implemented.');
+    } else if (command === 'ranks') {
+      const militaryRoles = getMilitaryRoles(message.guild);
+      if (militaryRoles.size > 0) {
+        const roleList = militaryRoles.map(role => role.name).join(', ');
+        await message.reply(`Military ranks found in this server: ${roleList}`);
+      } else {
+        await message.reply('No military ranks found in this server.');
+      }
     }
   }
 });
