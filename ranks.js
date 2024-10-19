8f97f225a0c0aa9b6c442a5f19091f6aa74d2536
JstrR6 <tigerspires13@gmail.com>
2024-10-19 12:40:26 -0500

    Create ranks.js

---
+++ ranks.js
@@ -0,0 +1,27 @@
+const militaryRanks = [
+  'Private',
+  'Private First Class',
+  'Specialist',
+  'Corporal',
+  'Sergeant',
+  'Staff Sergeant',
+  'Sergeant First Class',
+  'Master Sergeant',
+  'First Sergeant',
+  'Sergeant Major',
+  'Command Sergeant Major',
+  'Sergeant Major of the Armed Forces',
+  'Second Lieutenant',
+  'First Lieutenant',
+  'Captain',
+  'Major',
+  'Lieutenant Colonel',
+  'Colonel',
+  'Brigadier General',
+  'Major General',
+  'Lieutenant General',
+  'General',
+  'General of the Armed Forces'
+];
+
+module.exports = militaryRanks;
