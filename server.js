6f8a8f60aa6ee3e72a5410e2724b0fdc78981d6c
JstrR6 <tigerspires13@gmail.com>
2024-10-21 19:52:57 -0500

    Update

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express();
 
 // Serve static files from the React app's build directory
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Example API route (optional)
 app.get('/api/example', (req, res) => {
