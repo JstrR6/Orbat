4d910f1e16c029e8610fb62d4b18c1e3e9f7dc96
JstrR6 <tigerspires13@gmail.com>
2024-10-23 17:10:12 -0500

    Update

--- auth.js
+++ auth.js
@@ -55,13 +55,8 @@ const createOrUpdateUser = async (discordId, username, highestRole) => {
   }
 };
 
-passport.serializeUser((user, done) => {
-    done(null, user);
-});
-
-passport.deserializeUser((user, done) => {
-    done(null, user);
-});
+passport.serializeUser((user, done) => done(null, user));
+passport.deserializeUser((user, done) => done(null, user));
 
 passport.use(new DiscordStrategy({
     clientID: process.env.DISCORD_CLIENT_ID,
@@ -70,39 +65,24 @@ passport.use(new DiscordStrategy({
     scope: ['identify', 'guilds']
 }, async (accessToken, refreshToken, profile, done) => {
     try {
-        // Find the specific guild (server) in user's guilds
-        const userGuild = profile.guilds.find(g => g.id === process.env.DISCORD_GUILD_ID);
+        // Get user's server info
+        const guild = profile.guilds.find(g => g.id === process.env.DISCORD_GUILD_ID);
         
-        if (!userGuild) {
-            return done(null, false, { message: 'User is not in the required server' });
-        }
-
-        // Convert permissions number to roles
-        const userRoles = [];
-        if (userGuild.permissions) {
-            // Add roles based on permissions
-            if (userGuild.permissions & 0x8) userRoles.push('Administrator');
-            // Add more role checks as needed
-        }
-
+        // Save user to database
         let user = await User.findOne({ discordId: profile.id });
         if (!user) {
             user = new User({
                 discordId: profile.id,
                 username: profile.username,
-                roles: userRoles,
-                highestRole: userRoles[0] || null  // First role is typically highest
+                roles: guild ? guild.roles : [],
+                highestRole: guild ? guild.roles[0] : null
             });
-        } else {
-            user.roles = userRoles;
-            user.highestRole = userRoles[0] || null;
         }
-
         await user.save();
+        
+        // Done - go to dashboard
         return done(null, user);
-
     } catch (error) {
-        console.error('Auth Error:', error);
         return done(error, null);
     }
 }));
