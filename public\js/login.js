5b51a64b52eda3eb8d0e84847d0ed897676b4418
JstrR6 <tigerspires13@gmail.com>
2024-10-26 09:04:17 -0500

    Update

--- public\js/login.js
+++
@@ -1,5 +0,0 @@
-// Add any client-side login functionality here
-document.addEventListener('DOMContentLoaded', () => {
-    // You can add animations or additional functionality
-    console.log('Login page loaded');
-});
